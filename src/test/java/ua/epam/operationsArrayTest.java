bc32ddb6c67884fd6254b09617f59567e706cafa
azulw9 <azulw9@gmail.com>
2016-09-07 18:08:25 +0300

    add tests even/odd array size

--- src/test/java/ua/epam/operationsArrayTest.java
+++ src/test/java/ua/epam/operationsArrayTest.java
@@ -23,12 +23,19 @@ public class operationsArrayTest {
         int[] test_arr = a.createArray(size);
         int[] inv_arr = a.invertArray(test_arr);
         Assert.assertEquals(test_arr.length, inv_arr.length);
-
     }
 
     @Test
-    public void testCorrectInversion(){
+    public void testCorrectInversionEvenSize(){
         int[] test_arr = a.createArray(size);
+        int[] inv_arr = a.invertArray(test_arr);
+        int[] inv2_arr = a.invertArray(inv_arr);
+        Assert.assertArrayEquals(test_arr, inv2_arr);
+    }
+
+    @Test
+    public void testCorrectInversionOddSize(){
+        int[] test_arr = a.createArray(size+1);
         int[] inv_arr = a.invertArray(test_arr);
         int[] inv2_arr = a.invertArray(inv_arr);
         Assert.assertArrayEquals(test_arr, inv2_arr);
@@ -39,12 +46,13 @@ public class operationsArrayTest {
         int[] arr1 = a.createArray(size);
         Assert.assertEquals(size, arr1.length);
         Assert.assertNotNull(arr1);
-
     }
 
     @Test (expected = NegativeArraySizeException.class)
-    public void testCreateException(){
+    public void testNegativeArraySizeException(){
+
         int[] arr3 = a.createArray(-1);
+
     }
 
 }
